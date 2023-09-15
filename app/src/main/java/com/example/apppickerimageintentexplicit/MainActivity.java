4cd5d0845e237ecae2492e2e2f1b40a21b9bbd3d
Nam Phung <namhien12082003@gmail.com>
2023-09-15 21:17:06 +0700

    Initial commit

---
+++ app/src/main/java/com/example/apppickerimageintentexplicit/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.apppickerimageintentexplicit;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] mArrDrawable;
+    private ImageView mImgPick, mImgRandom;
+    private int mResourceRandom;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+        event();
+
+
+    }
+
+    private void event() {
+        mImgPick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                intent.putExtra("arr_drawable", mArrDrawable);
+                someActivityResultLauncher.launch(intent);
+            }
+        });
+    }
+
+    private ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        int resourceData = data.getIntExtra("resourceData", -1);
+                        mImgPick.setImageResource(resourceData);
+
+                        if (resourceData == mResourceRandom){
+                            Toast.makeText(MainActivity.this, "Bạn chọn chính xác", Toast.LENGTH_SHORT).show();
+                            mResourceRandom = randomImage(mArrDrawable);
+                            mImgRandom.setImageResource(mResourceRandom);
+                            mImgPick.setImageResource(R.drawable.nophoto);
+                        }else{
+                            Toast.makeText(MainActivity.this, "Bạn chọn chưa chính xác", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(MainActivity.this, "Bạn chưa chọn hình", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+    private void init() {
+        // Ánh xạ
+        mImgPick = findViewById(R.id.imgPick);
+        mImgRandom = findViewById(R.id.imgRandom);
+
+        // Tạo mảng chứa tên hình
+        mArrDrawable = getResources().getStringArray(R.array.ArrayDrawable);
+
+        // Xử lý việc gán hình cho hình random
+        mResourceRandom = randomImage(mArrDrawable);
+        mImgRandom.setImageResource(mResourceRandom);
+    }
+
+    private int randomImage(String[] arrDrawable) {
+        int index = new Random().nextInt(arrDrawable.length);
+        String name = arrDrawable[index];
+        int resourceImage = getResources().getIdentifier(name, "drawable", getPackageName());
+        return resourceImage;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.menu_item_refresh) {
+            mResourceRandom = randomImage(mArrDrawable);
+            mImgRandom.setImageResource(mResourceRandom);
+        }
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/apppickerimageintentexplicit/MainActivity2.java
@@ -0,0 +1,89 @@
+package com.example.apppickerimageintentexplicit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    TableLayout mTbLayout;
+    int mColumn, mRow, mCount;
+    int mWidthScreen, mHeightScreen;
+    int mResourceId;
+    String[] mArrDrawable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        mTbLayout = findViewById(R.id.tableLayout);
+
+        initView();
+    }
+
+    private void initView() {
+        // Get data
+        Intent intent = getIntent();
+        if (intent != null) {
+            mArrDrawable = intent.getStringArrayExtra("arr_drawable");
+            Collections.shuffle(Arrays.asList(mArrDrawable));
+        }
+
+        // Get dimension screen
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        mHeightScreen = displayMetrics.heightPixels;
+        mWidthScreen = displayMetrics.widthPixels;
+
+
+        mRow = (int) Math.ceil(Double.parseDouble((mArrDrawable.length / 3f) + ""));
+        mCount = 0;
+        mColumn = 3;
+        // 6 dong 3 cot
+        for (int i = 0; i < mRow; i++) {
+            //Create row
+            TableRow tableRow = new TableRow(this);
+            for (int y = 0; y < mColumn; y++) {
+                if (mCount < mArrDrawable.length) {
+                    // Tính vị trí của hình chữ nhật khi biết trục tung và hoành
+//                    mIndex =  mColumn * i  + y ;
+                    mResourceId = getResources().getIdentifier(mArrDrawable[mCount], "drawable", getPackageName());
+                    TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(mWidthScreen / 3 , mWidthScreen / 3);
+                    ImageView imageView = new ImageView(this);
+                    imageView.setImageResource(mResourceId);
+                    imageView.setTag(mResourceId);
+                    imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                    imageView.setLayoutParams(layoutParams);
+                    imageView.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intentData = new Intent();
+                            intentData.putExtra("resourceData",(int) imageView.getTag());
+                            setResult(Activity.RESULT_OK,intentData);
+                            finish();
+                        }
+                    });
+                    tableRow.addView(imageView);
+                    mCount++;
+                }
+            }
+            mTbLayout.addView(tableRow);
+        }
+    }
+}
